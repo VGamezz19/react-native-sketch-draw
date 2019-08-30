6a8a9b60067bd0b6043f8d70fae6a979b260657d
Simon Ermler <s.ermler@evosec.de>
2019-08-30 08:28:43 +0200

    use androidx instead of support library

--- android/src/main/java/com/reactlibrary/RNSketchViewManager.java
+++ android/src/main/java/com/reactlibrary/RNSketchViewManager.java
@@ -2,7 +2,7 @@
 package com.reactlibrary;
 
 import android.graphics.Color;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.View;
 
 import com.facebook.infer.annotation.Assertions;
